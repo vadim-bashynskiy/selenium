a6e2f85dcdebf41e6ba290ca2a4eba72db385e08
vadim-bashynskiy <underklon@mail.ru>
2017-07-02 19:45:39 +0300

    Merge branch 'master' of C:\selenium with conflicts.

--- src/test/java/com/kit/google/homework/lesson3/TomCook.java
+++ src/test/java/com/kit/google/homework/lesson3/TomCook.java
@@ -2,15 +2,10 @@ package com.kit.google.homework.lesson3;
 
 import com.kit.core.WebDriverTestBase;
 import com.kit.util.WebDriverUtil;
-import org.openqa.selenium.*;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebElement;
 import org.testng.annotations.Test;
-
-import java.util.List;
-
-import static org.testng.Assert.assertTrue;
-import static org.testng.AssertJUnit.assertFalse;
 
 /**
  * Created by testu on 6/20/2017.
@@ -18,24 +13,11 @@ import static org.testng.AssertJUnit.assertFalse;
 public class TomCook extends WebDriverTestBase{
     @Test
     public void test() throws InterruptedException {
-
         WebDriverUtil webDriverUtil = new WebDriverUtil(webDriver);
+        String g = "Hello";
         webDriver.get("https://www.thomascook.com/");
-
-
-        List<WebElement> webElementList = webDriver.findElements(By.cssSelector(".SearchbarForm-summary.ng-binding"));
-        if (!webElementList.isEmpty()) {
-            webElementList.get(1).click();
-        }else {
-            assertFalse("Elements not found", true);
-        }
-
-        //simple case with java script
-       // WebElement element = webDriver.findElement(By.cssSelector(".SearchbarForm-summary.ng-binding"));
-      //   element.click();
-        webDriverUtil.jsClick("SearchbarForm-origin","id");
-        webDriverUtil.waitForExpectedCondition(ExpectedConditions.visibilityOfElementLocated(By.className("deliver-component-CloseButton-times"))).click();
+        WebElement element = webDriver.findElement(By.id("SearchbarForm-origin"));
+       // element.sendKeys("Hello");
+        webDriverUtil.jsClick("SearchbarForm-goingTo","id");
     }
-   /* document.getElementsByClassName("deliver-component-Modal")[0].remove()
-        document.getElementsByClassName("deliver-overlay")[0].remove()*/
 }
